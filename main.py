6ccf47bd6ab5faadc2400602759c9b9750684e96
No2ta <abdelrahmanashraf1987@gmail.com>
2025-08-20 21:28:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import tkinter
 from tkinter import *
 from PIL import ImageTk, Image
 from tkinter import messagebox
@@ -9,6 +10,10 @@ root.title('to do list???')
 root.iconbitmap('icon.ico')
 root.attributes('-fullscreen', True)
 root.bind("<Escape>", lambda e: root.attributes("-fullscreen", False))
+root.bind("<Tab>", lambda e: root.attributes("-fullscreen", True))
+root.configure(bg="#c58d40")
+listbox_task = tkinter.Listbox(root, bg="#c8954c" ,height=25, width=100)
+listbox_task.pack()
 
 
 
